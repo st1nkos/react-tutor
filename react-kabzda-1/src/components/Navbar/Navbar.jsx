4d86d6c6d6cccc81c6b1e659b23508b9adc5bf04
AntonZhdanov <zdnn182@hotmail.com>
2020-12-17 16:26:36 +0500

    route+navlink

--- react-kabzda-1/src/components/Navbar/Navbar.jsx
+++ react-kabzda-1/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
+
 const Navbar  = ()=>{
   return(
       <nav className={s.nav}>
-      <div className={`${s.item} ${s.active}`}><a>Profile</a></div>
-      <div className={s.item}><a>Messages</a></div>
-      <div className={s.item}><a>News</a></div>
-      <div className={s.item}><a>Music</a></div>
-      <div className={s.item}><a>Setings</a></div>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
+        </div>
+      <div className={s.item}>
+        <NavLink to='/dialogs' activeClassName={s.activeLink} >Messages</NavLink>
+        </div>
+      <div className={s.item}>
+        <NavLink to='/news' activeClassName={s.activeLink}>News</NavLink>
+        </div>
+      <div className={s.item}>
+        <NavLink to='/music' activeClassName={s.activeLink}>Music</NavLink>
+        </div>
+      <div className={s.item}>
+        <NavLink to='/setings' activeClassName={s.activeLink}>Setings</NavLink>
+        </div>
     </nav>
   );
 }
--- react-kabzda-1/src/components/Profile/Profile.jsx
+++ react-kabzda-1/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts';
 import s from'./Profile.module.css';
 const Profile  = ()=>{
   return(
-    <div className={s.content}>
+    <div>
     <div>
       <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
     </div>
