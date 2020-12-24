a02ed83afdb22e4db16c5451aca3bf496323f965
AntonZhdanov <zdnn182@hotmail.com>
2020-12-24 15:18:46 +0500

    texarea in profile FLUX on all simbols

--- react-kabzda-1/src/redux/state.js
+++ react-kabzda-1/src/redux/state.js
@@ -8,6 +8,7 @@ let state = {
             { id: 3, message: "blala", likesCount: 11 },
             { id: 4, message: "dadad", likesCount: 11 }
         ],
+        newPostText: 'blabla'
     },
 
     dialogsPage: {
@@ -29,15 +30,23 @@ let state = {
 
 }
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
     let newPost = {
         id: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 0,
     }
 
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
     rerenderEntireTree(state);
 }
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
+}
+
+
+
 
 export default state
