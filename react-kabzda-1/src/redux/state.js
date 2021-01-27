10002b4f24c7b16676ce0a9b8a5e87b863530cb5
AntonZhdanov <zdnn182@hotmail.com>
2021-01-27 10:39:23 +0500

    subscribe(observer)

--- react-kabzda-1/src/redux/state.js
+++ react-kabzda-1/src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from './../render';
+let rerenderEntireTree = () => {
+    console.log('State changed');
+}
 
 let state = {
     profilePage: {
@@ -30,7 +32,9 @@ let state = {
 
 }
 
-export let addPost = () => {
+window.state = state
+
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -41,9 +45,13 @@ export let addPost = () => {
     state.profilePage.newPostText = '';
     rerenderEntireTree(state);
 }
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscibe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 
